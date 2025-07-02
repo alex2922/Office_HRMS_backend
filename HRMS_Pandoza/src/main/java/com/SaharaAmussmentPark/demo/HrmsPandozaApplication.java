34470662b5391e7599c00e07b01d642e2a528d4f
Prajwal <prajwalkulkarni169@gmail.com>
2025-07-02 14:45:34 +0530

    changes for eureka client

--- HRMS_Pandoza/src/main/java/com/SaharaAmussmentPark/demo/HrmsPandozaApplication.java
+++ HRMS_Pandoza/src/main/java/com/SaharaAmussmentPark/demo/HrmsPandozaApplication.java
@@ -3,7 +3,7 @@ package com.SaharaAmussmentPark.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com.SaharaAmussmentPark")
 @EntityScan("com.SaharaAmussmentPark.model")
 @EnableJpaRepositories("com.SaharaAmussmentPark.Repository")
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class HrmsPandozaApplication {
 
 	public static void main(String[] args) {
